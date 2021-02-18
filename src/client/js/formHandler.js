3ac4519a9f8b2249e036f1a9f94a3489fee43693
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-02-18 15:26:57 +0000

    Custom API call challenge
    Here I've changed the api call to be a request to the weather api from the previous project.
    
    Now after submitting the form, the weather in Beverly Hills gets displayed.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,10 +6,13 @@ function handleSubmit(event) {
     Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    // fetch('http://localhost:8081/test')
+    //fetch request for weather of zip code 90210
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=90210,US&appid=c385c18d161b14df158500c5452637e5')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = 'the temperature in Beverly Hills is : ' + res.main.temp
+        console.log(res.main.temp)
     })
 }
 
